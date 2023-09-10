71ab531d06ca613c4a2ec4c9ccaadae53681365d
Margulis162 <markmargulis@proton.me>
2023-09-10 19:34:10 -0400

    randomizing function for a range

---
+++ script.js
@@ -0,0 +1,10 @@
+// vars
+const btns = document.getElementsByClassName("btn");
+
+
+// f(x)
+function rand_gen(min, max){
+    num = Math.floor(Math.random()*(max - min + 1) + min);
+    
+}
+// flow
